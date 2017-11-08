6098dc705b3a1c495d5f0e9d9cc8c2264083229e
Jack Muratore <jackmuratore@gmail.com>
2017-11-08 00:57:03 -0500

    Jumping and moving added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public float VerticalSpeed = 3.0f;
+
+	private Rigidbody2D rb2D;
+	// Use this for initialization
+	void Start ()
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			rb2D.AddForce(transform.up * 300);
+		}
+		var verticalDirection = 0;
+		if (Input.GetKey(KeyCode.A))
+		{
+			verticalDirection -= 1;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			verticalDirection += 1;
+		}
+		var xVelocity = verticalDirection * VerticalSpeed;
+		var velocity = rb2D.velocity;
+		velocity.x = xVelocity;
+		rb2D.velocity = velocity;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+This is a platform learning project.
 
+All awesome sprite art was created by www.kenney.nl
+
+From https://opengameart.org/content/platformer-art-complete-pack-often-updated
